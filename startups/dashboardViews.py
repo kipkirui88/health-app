4a97962d3e8aa2d5861f31d4942c06dedb8b553d
kipkirui koech <106087508+kipkirui88@users.noreply.github.com>
2023-06-13 05:46:04 +0300

    Update dashboardViews.py
    change views

--- startups/dashboardViews.py
+++ startups/dashboardViews.py
@@ -2,8 +2,6 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import User
 from django.http import HttpResponseRedirect
-
-from .models import Startups, Job
 from .forms import StartupsForm, JobForm
 
 # Create your views here.
@@ -25,32 +23,7 @@ def index(request):
         form=StartupsForm()
         img=Startups.objects.all()
         return render(request, 'post_startup.html',{'img':img, 'form':form})
-                 
-def jobs(request):
-    if request.method == "POST":
-        form = JobForm(data=request.POST, files=request.FILES)
-        if form.is_valid():
-            form.save()
-            obj=form.instance
-        return render(request, 'index.html',{'obj':obj})
-    else:
-        form=JobForm()
-        img=Job.objects.all()
-        return render(request, 'post_job.html',{'img':img, 'form':form})
-   
-
-def profile_update(request):
-    if request.method == 'POST':
-        email = request.POST['email']
-        first_name = request.POST['firstname']
-        last_name = request.POST['lastname']
-        user = User.objects.get(id=request.user.id)
-        user.email = email
-        user.firstname = first_name
-        user.lastname = last_name
-        user.save()
-        print('user updated')
-        return redirect('/profile') 
+              
 
 def Logout(request):
     return redirect('/')
