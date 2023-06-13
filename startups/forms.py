c975b981ab2f8bd2bbf573c0bd3e7dad84a1e087
kipkirui koech <106087508+kipkirui88@users.noreply.github.com>
2023-06-13 05:46:58 +0300

    Update forms.py
    remove some fields in forms

--- startups/forms.py
+++ startups/forms.py
@@ -8,25 +8,5 @@ class JobForm(forms.ModelForm):
         fields = ("title","company","contact","location","description","company_logo","date")
         widgets ={
             'title':forms.TextInput(attrs={'placeholder':'Name', 'style': 'width: 60%;left:300px', 'class': 'form-control'}),
-            'company':forms.TextInput(attrs={'placeholder':'Name', 'style': 'width: 60%;', 'class': 'form-control'}),
-            'contact':forms.NumberInput(attrs={'placeholder':'Name', 'style': 'width: 60%;', 'class': 'form-control'}),
-            'location':forms.TextInput(attrs={'placeholder':'Name', 'style': 'width: 60%;', 'class': 'form-control'}),
-            'description':forms.Textarea(attrs={'placeholder':'Name', 'style': 'width: 60%;', 'class': 'form-control'}),
-            # 'company_logo':forms.TextInput(attrs={'placeholder':'Name', 'style': 'width: 60%;', 'class': 'form-control'}),
-            'date':forms.DateInput(attrs={'placeholder':'Format:YYYY-MM-DD', 'style': 'width: 60%;', 'class': 'form-control'}),
+               'date':forms.DateInput(attrs={'placeholder':'Format:YYYY-MM-DD', 'style': 'width: 60%;', 'class': 'form-control'}),
         }
-class StartupsForm(forms.ModelForm):
-    # name = forms.CharField(widget=forms.TextInput(attrs={'placeholder':'Name', 'style': 'width: 40%;', 'class': 'form-control'}))
-    # contact = forms.IntegerField(widget=forms.TextInput(attrs={'placeholder':'Name', 'style': 'width: 40%;', 'class': 'form-control'}))
-    class Meta:
-        model = Startups
-        fields = ("name","specialty","contact","location","description","image","website")
-        widgets ={
-            'name':forms.TextInput(attrs={'placeholder':'Name', 'style': 'width: 60%;', 'class': 'form-control'}),
-            'contact':forms.NumberInput(attrs={'placeholder':'Name', 'style': 'width: 60%;', 'class': 'form-control'}),
-            'specialty':forms.TextInput(attrs={'placeholder':'Name', 'style': 'width: 60%;', 'class': 'form-control'}),
-            'location':forms.TextInput(attrs={'placeholder':'Name', 'style': 'width: 60%;', 'class': 'form-control'}),
-            'description':forms.Textarea(attrs={'placeholder':'Name', 'style': 'width: 60%;', 'class': 'form-control'}),
-            # 'image':forms.TextInput(attrs={'placeholder':'Name', 'style': 'width: 60%;', 'class': 'form-control'}),
-            'website':forms.TextInput(attrs={'placeholder':'Name', 'style': 'width: 60%;', 'class': 'form-control'}),
-        }        
